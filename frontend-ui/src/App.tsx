9a786d81aa36b6e8693a2dd5d769697bbe19a3f0
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-06-08 21:19:11 +0300

    pressin MVP

--- frontend-ui/src/App.tsx
+++ frontend-ui/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [presses, setPresses] = useState(0);
+
+  const handlePress = () => {
+    setPresses(presses + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Press the button</h1>
+      <p>Button has been pressed {presses} times</p>
+      <button onClick={handlePress}>The button</button>
     </div>
   );
 }
