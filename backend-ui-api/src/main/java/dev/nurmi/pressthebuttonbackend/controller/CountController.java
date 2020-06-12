03a6c541946d10a992687aaf7347e37cfcca76e8
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2020-06-12 23:07:38 +0300

    backend accessable via proxy to frontend

--- backend-ui-api/src/main/java/dev/nurmi/pressthebuttonbackend/controller/CountController.java
+++ backend-ui-api/src/main/java/dev/nurmi/pressthebuttonbackend/controller/CountController.java
@@ -5,14 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.lang.Math;
-
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api/v1")
 public class CountController {
 
+    Logger logger = LoggerFactory.getLogger(CountController.class);
+
     @GetMapping("/count")
     public int getCount(){
+        logger.info("/count called");
         return (int) (Math.random() * 100) + 1;
     }
 
